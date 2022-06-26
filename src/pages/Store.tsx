b9322b2d7b5b060ce222688907cd4fa31a5b05b2
Adan Abbi <adanweli@hotmail.com>
2022-06-25 21:44:47 -0700

    added components

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,7 +1,17 @@
+import storeItem from "../data/items.json";
+import { Col, Row } from "react-bootstrap";
+import { StoreItem } from "../components/StoreItem";
+
 export function Store() {
   return (
-    <div>
-      <h1>THIS IS STORE PAGE</h1>
-    </div>
+    <>
+      <Row md={2} xs={1} lg={3} className='g-3'>
+        {storeItem.map((item) => (
+          <Col key={item.id}>
+            <StoreItem {...item} />
+          </Col>
+        ))}
+      </Row>
+    </>
   );
 }
